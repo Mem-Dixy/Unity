a36d0e918c2cecd00b74ebaf585f37dafea723dd
mem_dixy <mem_dixy@pm.me>
2021-08-13 02:42:21 +0000

    save point

--- Tank_Game/Assets/Universal Assets/WorldBoundary.cs
+++ Tank_Game/Assets/Universal Assets/WorldBoundary.cs
@@ -1,16 +1,13 @@
-using UnityEngine;
-using System.Collections;
-
-public class WorldBoundary : MonoBehaviour {
+public class WorldBoundary : UnityEngine.MonoBehaviour {
 	// If worried about it going so fast it flys out of level then add an array that stores objects that fly away
 	// and if they don't enter into sphere again then delete them after a short pause;
-	private void OnTriggerExit (Collider other) {
+	private void OnTriggerExit (UnityEngine.Collider other) {
 		PlayerBasic basic = other.GetComponent(typeof(PlayerBasic)) as PlayerBasic;
 		if (basic) {
 			basic.ResetPosition();
 		}
 		else {
-			Destroy(other.gameObject);
+			UnityEngine.Object.Destroy(other.gameObject);
 		}
 	}
 }
