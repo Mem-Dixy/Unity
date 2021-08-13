81077a54b4cc22d93f39b10789b4a6d4c760dd26
mem_dixy <mem_dixy@pm.me>
2021-08-13 02:44:50 +0000

    Stacatto update.

--- Low Poly Cars/Assets/Scripts/Car.cs
+++ Low Poly Cars/Assets/Scripts/Car.cs
@@ -1,41 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
-public class Car : MonoBehaviour {
-	public WheelCollider wheelColliderLeftFront;
-	public WheelCollider wheelColliderLeftBack;
-	public WheelCollider wheelColliderRightFront;
-	public WheelCollider wheelColliderRightBack;
+public class Car : MonoBehaviour
+{
+    public Transform centerOfMass;
+    
+    public WheelCollider wheelColliderLeftFront;
+    public WheelCollider wheelColliderLeftBack;
+    public WheelCollider wheelColliderRightFront;
+    public WheelCollider wheelColliderRightBack;
 
-	public Transform wheelLeftFront;
-	public Transform wheelRightFront;
-	public Transform wheelLeftBack;
-	public Transform wheelRightBack;
+    public Transform wheelLeftFront;
+    public Transform wheelRightFront;
+    public Transform wheelLeftBack;
+    public Transform wheelRightBack;
 
-	public float motorTorque = 100f;
-	public float maxSteer = 20f;
+    public float motorTorque = 100f;
+    public float maxSteer = 20f;
+    private Rigidbody _rigidbody;
 
-	void FixedUpdate() {
-		wheelColliderLeftBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-		wheelColliderRightBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-		wheelColliderLeftFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-		wheelColliderRightFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-	}
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = centerOfMass.localPosition;
+    }
 
-	void Update() {
-        WheelTurn(wheelColliderLeftFront, wheelLeftFront);
-        WheelTurn(wheelColliderRightFront, wheelRightFront);
-        WheelTurn(wheelColliderLeftBack, wheelLeftBack);
-        WheelTurn(wheelColliderRightBack, wheelRightBack);
-	}
+    void FixedUpdate()
+    {
+        wheelColliderLeftBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        wheelColliderRightBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+     
+        wheelColliderLeftFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        wheelColliderRightFront.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
 
-	private void WheelTurn(UnityEngine.WheelCollider collider , UnityEngine.Transform wheel) {
-		UnityEngine.Vector3 pos = Vector3.zero;
-		UnityEngine.Quaternion quat = Quaternion.identity;
-		collider.GetWorldPose(out pos , out quat);
-		wheel.position = pos;/
-		wheel.rotation = quat;
-	}
+    }
+
+    void Update()
+    {
+        var pos = Vector3.zero;
+        var rot = Quaternion.identity;
+
+        wheelColliderLeftFront.GetWorldPose(out pos, out rot);
+        wheelLeftFront.position = pos;
+        wheelLeftFront.rotation = rot * Quaternion.Euler(0, 180, -90);
+
+        wheelColliderRightFront.GetWorldPose(out pos, out rot);
+        wheelRightFront.position = pos;
+        wheelRightFront.rotation = rot * Quaternion.Euler(0,180,90);
+
+        wheelColliderLeftBack.GetWorldPose(out pos, out rot);
+        wheelLeftBack.position = pos;
+        wheelLeftBack.rotation = rot * Quaternion.Euler(0, 180, -90);
+
+        wheelColliderRightBack.GetWorldPose(out pos, out rot);
+        wheelRightBack.position = pos;
+        wheelRightBack.rotation = rot * Quaternion.Euler(0, 180,90);
+    }
 }
