a38d4df76669831fa6c696ebbdd9f23059048958
mem_dixy <mem_dixy@pm.me>
2021-09-04 07:54:20 +0000

    Input Package and UI Package don't work together.

--- EnTropy/Assets/UI/AlignmentTags.cs
+++ EnTropy/Assets/UI/AlignmentTags.cs
@@ -1,34 +1,28 @@
 using UnityEngine.UIElements;
 using UnityEngine;
 
-namespace Samples.Runtime.Text
-{
-    [RequireComponent(typeof(UIDocument))]
-    public class AlignmentTags : MonoBehaviour
-    {
-        [SerializeField] private PanelSettings panelSettings = default;
-        [SerializeField] private VisualTreeAsset sourceAsset = default;
-        [SerializeField] private StyleSheet styleAsset = default;
+namespace Samples.Runtime.Text {
+	[RequireComponent(typeof(UIDocument))]
+	public class AlignmentTags : MonoBehaviour {
+		[SerializeField] private PanelSettings panelSettings = default;
+		[SerializeField] private VisualTreeAsset sourceAsset = default;
+		[SerializeField] private StyleSheet styleAsset = default;
+		void Awake() {
+			UIDocument uiDocument = this.GetComponent<UIDocument>();
+			uiDocument.panelSettings = this.panelSettings;
+			uiDocument.visualTreeAsset = this.sourceAsset;
+		}
+		void OnEnable() {
+			this.InitializeVisualTree(this.GetComponent<UIDocument>());
+		}
+		private void InitializeVisualTree(UIDocument uiDocument) {
+			VisualElement root = uiDocument.rootVisualElement;
+			root.styleSheets.Add(this.styleAsset);
 
-        void Awake()
-        {
-            var uiDocument = GetComponent<UIDocument>();
-            uiDocument.panelSettings = panelSettings;
-            uiDocument.visualTreeAsset = sourceAsset;
-        }
-
-        void OnEnable()
-        {
-            InitializeVisualTree(GetComponent<UIDocument>());
-        }
-
-        private void InitializeVisualTree(UIDocument uiDocument)
-        {
-            VisualElement root = uiDocument.rootVisualElement;
-            root.styleSheets.Add(styleAsset);
-
-            root.Query(className: "disableRichText").ForEach(
-                (element => (element as Label).enableRichText = false));
-        }
-    }
+			_ = root.Query(className: "disableRichText").ForEach(
+				element => {
+					return (element as Label).enableRichText = false;
+				});
+		}
+	}
 }
